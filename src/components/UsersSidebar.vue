6c55e5e6b7ef19f43d0ba3ea782bde687f4cb124
Vinicius-Muller <viniciusmul98@gmail.com>
2024-07-18 19:01:42 -0300

    init

---
+++ src/components/UsersSidebar.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    :location="$vuetify.display.mobile ? 'bottom' : 'right'"
+    width="500"
+  >
+    <v-list
+      :items="items"
+    ></v-list>
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts">
+ export default {
+    data: () => ({
+      drawer: false,
+      group: null,
+      items: [
+        {
+          title: 'Foo',
+          value: 'foo',
+        },
+        {
+          title: 'Bar',
+          value: 'bar',
+        },
+        {
+          title: 'Fizz',
+          value: 'fizz',
+        },
+        {
+          title: 'Buzz',
+          value: 'buzz',
+        },
+      ],
+    }),
+
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+  }
+</script>
+
+<style>
+
+</style>
